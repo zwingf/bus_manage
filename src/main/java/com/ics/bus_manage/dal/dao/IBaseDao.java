98db3b2ae1e37dc11201d8a9225efae3556c5d73
张羽飞 <wing639871122@163.com>
2017-08-07 12:32:25 +0800

    添加IbaseDao接口使用说明

--- src/main/java/com/ics/bus_manage/dal/dao/IBaseDao.java
+++ src/main/java/com/ics/bus_manage/dal/dao/IBaseDao.java
@@ -12,6 +12,11 @@ public interface IBaseDao<T> {
 
     List<T> getList(T t1, T t2, Object obj);
 
+    /**
+     *通过自定义查询参数获取所有封装实体类列表
+     * @param obj 例如可以传入一个Object参数，然后dao重写getDetachedCriteria()方法实现参数解析
+     * @return
+     */
     List<T> getList(Object obj);
 
     void add(T t);
